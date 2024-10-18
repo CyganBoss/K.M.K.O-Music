2c5e9baf2b97c1866c300866e87c4204296250f4
CyganBoss <jestemsuperkoxem123@gmail.com>
2024-10-19 00:58:00 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 
 module.exports = {
-    token: '',
+    token: 'MTI5Njk1NDgwOTc2MDYxNjQ2OA.GV_9cL.vadu8bz9w_9Ny8nTG970U4O8lE_wW3RPvXNLNI',
     clientId: '1273230302822797312',
     guildId: '1106591466651922502',
-    youtubeApiKey: process.env.YOUTUBE_API_KEY,
-    mongoUri: process.env.MONGO_URI,
+    youtubeApiKey: process.env.AIzaSyA6hSUWChyjZk7zcmZZZ6RgGd8-Pq8TSLw,
+    mongoUri: process.env.mongodb+srv://klaudiazbuczyn4:<uxWQnz0EGgNB3OzX>@cluster0.i0obh.mongodb.net/,
     spotify: {
         clientId: process.env.SPOTIFY_CLIENT_ID,
         clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
